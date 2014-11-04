c6780d4121f862f769c452805a537ae9a7279135
Yxwww <yuxi.wangcd@gmail.com>
2014-11-04 15:14:52 -0700

    init

---
+++ src/main/java/cpsc441/solution/Main.java
@@ -0,0 +1,23 @@
+package cpsc441.solution;
+
+import cpsc441.doNOTmodify.ILogFactory;
+import cpsc441.doNOTmodify.IUDPSocket;
+import cpsc441.doNOTmodify.LogFactory;
+import cpsc441.doNOTmodify.UDPSocket;
+
+import java.net.DatagramSocket;
+
+public class Main {
+
+    public static void main(String [] args) {
+        DatagramSocket s = null;
+
+        //You shouldn't need to modify this code.
+        IUDPSocket sock = new UDPSocket(s);
+        ILogFactory logFactory = new LogFactory();
+        Router r = new Router(sock, logFactory);
+        r.run();
+    }
+
+
+}
---
+++ src/main/java/cpsc441/solution/Router.java
@@ -0,0 +1,15 @@
+package cpsc441.solution;
+
+import cpsc441.doNOTmodify.ILogFactory;
+import cpsc441.doNOTmodify.IUDPSocket;
+
+public class Router implements Runnable {
+
+    public Router(IUDPSocket sock, ILogFactory logFactory) {
+
+    }
+
+    public void run() {
+
+    }
+}
